6b5d44a642ea491ccbd578f64e2f6a3955decd79
nouval m abdul rojak <nouval.m@mhs.itenas.ac.id>
2025-06-10 17:03:08 +0700

    fix jwt

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -74,7 +74,7 @@ class AuthController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => Auth::factory()->getTTL() * 60 // Waktu dalam detik
+            'expires_in' => auth('api')->factory()->getTTL() * 60 // Waktu dalam detik
         ]);
     }
 }
